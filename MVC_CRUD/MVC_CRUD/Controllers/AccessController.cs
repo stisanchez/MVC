e84d3e2cd45ee5374ae4d5bb997845e345dcebba
Stiven Sanchez <stsanchez92@gmail.com>
2020-01-06 10:50:39 -0600

    First Tutorial

---
+++ MVC_CRUD/MVC_CRUD/Controllers/AccessController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_CRUD.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string user, string pass)
+        {
+            try
+            {
+                return Content("1");
+            }catch(Exception error)
+            {
+                return Content("Ocurrió un error :( " + error.Message);
+            }
+        }
+    }
+}
